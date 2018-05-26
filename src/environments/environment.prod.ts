2611738d3c25a14cc4b731e1759488c4781c8f7d
sloan <sloan@hotmail.com>
2018-05-26 13:53:22 +0200

    project init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBTtwt-yXTq8c4XIFz--z4eonEHUMULagM",
+    authDomain: "fir-chat-app-75dcc.firebaseapp.com",
+    databaseURL: "https://fir-chat-app-75dcc.firebaseio.com",
+    projectId: "fir-chat-app-75dcc",
+    storageBucket: "fir-chat-app-75dcc.appspot.com",
+    messagingSenderId: "822903962941"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBTtwt-yXTq8c4XIFz--z4eonEHUMULagM",
+    authDomain: "fir-chat-app-75dcc.firebaseapp.com",
+    databaseURL: "https://fir-chat-app-75dcc.firebaseio.com",
+    projectId: "fir-chat-app-75dcc",
+    storageBucket: "fir-chat-app-75dcc.appspot.com",
+    messagingSenderId: "822903962941"
+  }
+};
